281abe2c4ac77bc4545f16cee1d1f253e07ae408
Professeur Gibaud <mail@professeurgibaud.ovh>
2019-12-16 14:07:08 +0100

    Amélioration lisibilité csv

--- HAVP-Lire-Csv.py
+++ HAVP-Lire-Csv.py
@@ -5,10 +5,15 @@ import os
 def dossier():
     os.chdir("Google Drive\Python\Twitter HAVP\Twitter-HAVP")
 
-
+dossier()
 Dico = {}    
+Liste =[]
+i=0
 with open("liste.csv","r") as file:
     reader = csv.DictReader(file)
     for row in reader :
-        print(dict(row))
+        i = i+1
+        print(i)
+        Liste.append(dict(row))
+        
     
